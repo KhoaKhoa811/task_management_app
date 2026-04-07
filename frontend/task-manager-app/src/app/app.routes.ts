a0862d082a69f490354af410de64a5aa3ff55071
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2026-04-07 23:25:28 +0700

    refactor: implement header for frontend

---
+++ frontend/task-manager-app/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    // {
+    //     path: 'kanban',
+    //     loadComponent: () => import('./features/kanban/kanban.ts').then(m => m.KanbanBoard)
+    // },
+    // {
+    //     path: 'calendar',
+    //     loadComponent: () => import('./features/calendar/calendar.ts').then(m => m.CalendarView)
+    // },
+    // { path: '', redirectTo: 'kanban', pathMatch: 'full' },
+    // { path: '**', loadComponent: () => import('./features/not-found/not-found.ts').then(m => m.NotFound) }
+];
