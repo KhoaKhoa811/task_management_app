bc48d0b0a6d80780f41049f1d10461691b8b545f
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2026-04-14 22:35:13 +0700

    refactor: implement line chart

--- frontend/task-manager-app/src/app/features/chart-board/chart-board.ts
+++ frontend/task-manager-app/src/app/features/chart-board/chart-board.ts
@@ -21,30 +21,66 @@ export class ChartBoard {
 
   public pieChartData: ChartData<'doughnut'> = {
     labels: ['Backlog', 'Todo', 'In Progress', 'Done', 'Cancelled'],
-    datasets: [{
-      data: [10, 20, 15, 25, 5],
-      
-      backgroundColor: [
-        '#94a3b8', 
-        '#60a5fa', 
-        '#fbbf24', 
-        '#4ade80', 
-        '#f87171'  
-      ],
+      datasets: [{
+        data: [10, 20, 15, 25, 5],
+        
+        backgroundColor: [
+          '#94a3b8', 
+          '#60a5fa', 
+          '#fbbf24', 
+          '#4ade80', 
+          '#f87171'  
+        ],
 
-      hoverBackgroundColor: [
-        '#64748b', 
-        '#2563eb', 
-        '#f59e0b', 
-        '#16a34a', 
-        '#dc2626'
-      ],
+        hoverBackgroundColor: [
+          '#64748b', 
+          '#2563eb', 
+          '#f59e0b', 
+          '#16a34a', 
+          '#dc2626'
+        ],
 
-      borderColor: '#ffffff',
-      borderWidth: 2,
-      
-      hoverBorderColor: '#ffffff',
-      hoverBorderWidth: 3
-  }]
-};
+        borderColor: '#ffffff',
+        borderWidth: 2,
+        
+        hoverBorderColor: '#ffffff',
+        hoverBorderWidth: 3
+    }]
+  };
+
+  public lineChartType: ChartType = 'line';
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: { display: true }
+      },
+      x: {
+        grid: { display: false }
+      }
+    },
+    plugins: {
+      legend: { display: true, position: 'bottom' }
+    }
+  };
+
+  public lineChartData: ChartData<'line'> = {
+    labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6'],
+    datasets: [
+      {
+        data: [2, 4, 3, 5, 7, 3],
+        label: 'Overdue Tasks',
+        borderColor: '#3b82f6',
+        backgroundColor: 'rgba(59, 130, 246, 0.1)', 
+        fill: 'origin', 
+        tension: 0.4,
+        pointRadius: 5,
+        pointHoverRadius: 7,
+        pointBackgroundColor: '#3b82f6'
+      }
+    ]
+  };
 }
